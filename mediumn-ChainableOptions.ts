c80efabd5d0e43e906b0dd11a6456e29fd83f4f4
BBboy01 <1156678002@qq.com>
2022-01-26 20:07:11 +0800

    .

---
+++ mediumn-ChainableOptions.ts
@@ -0,0 +1,23 @@
+namespace ChainableOptions {
+  type Chainable = {
+    option: (key: string, type: any) => Chainable;
+    get: () => any;
+  };
+
+  declare const config: Chainable;
+
+  const result = config
+    .option("foo", 123)
+    .option("name", "type-challenges")
+    .option("bar", { value: "Hello World" })
+    .get();
+
+  // expect the type of result to be:
+  interface Result {
+    foo: number;
+    name: string;
+    bar: {
+      value: string;
+    };
+  }
+}
